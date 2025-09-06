4577a39fda311791e20f6dc3f26d2edb78292810
Pratyush-Raj7 <rpratyush163@gmail.com>
2025-09-06 13:48:00 +0530

    Updated home section and styles

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Theme toggler
 const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
@@ -9,8 +8,6 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.textContent = "🌙";
   }
 });
-
-// Form submit (just a demo)
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Thank you for contacting us! We'll get back to you soon.");
